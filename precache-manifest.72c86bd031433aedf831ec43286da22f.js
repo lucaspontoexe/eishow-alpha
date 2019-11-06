7d967ccc48ffe765356e6af3fd0c76d0b7924d72
lucaspontoexe <ltx16@hotmail.com>
2019-11-05 23:37:40 -0300

    Updates

---
+++ precache-manifest.72c86bd031433aedf831ec43286da22f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c56a2c670c850712a4b26fdbd6d0d16",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3180b4011317c0160261",
+    "url": "./static/css/main.16256562.chunk.css"
+  },
+  {
+    "revision": "1380515d58fc3b7f56ec",
+    "url": "./static/js/2.8f4726ab.chunk.js"
+  },
+  {
+    "revision": "3180b4011317c0160261",
+    "url": "./static/js/main.bcad1dc0.chunk.js"
+  },
+  {
+    "revision": "b13ad58ade045c525740",
+    "url": "./static/js/runtime-main.eda09d9b.js"
+  },
+  {
+    "revision": "eeccebcca5a4efbe1da731ab04fa02a9",
+    "url": "./static/media/back.eeccebcc.svg"
+  },
+  {
+    "revision": "45520ac91b09988df7feb8aa0c253660",
+    "url": "./static/media/eishow-lite.45520ac9.webm"
+  },
+  {
+    "revision": "8e3bf7af00cc2b50b17f8eb0776e05cd",
+    "url": "./static/media/logo.8e3bf7af.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eishow-alpha/precache-manifest.a1724e771a4cb78382fa64143cc80f8c.js"
+  "./precache-manifest.72c86bd031433aedf831ec43286da22f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eishow-alpha/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
