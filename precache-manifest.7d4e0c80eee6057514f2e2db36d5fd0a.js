a3a6de02f083e0a164bedc5c72a2875e7ee216c6
lucaspontoexe <ltx16@hotmail.com>
2019-11-05 23:26:10 -0300

    Updates

---
+++ precache-manifest.7d4e0c80eee6057514f2e2db36d5fd0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3262d60bb133ff22dbea4e937ed29639",
+    "url": "/eishow-alpha/index.html"
+  },
+  {
+    "revision": "364fc99a169882cbdd2e",
+    "url": "/eishow-alpha/static/css/main.16256562.chunk.css"
+  },
+  {
+    "revision": "1380515d58fc3b7f56ec",
+    "url": "/eishow-alpha/static/js/2.8f4726ab.chunk.js"
+  },
+  {
+    "revision": "364fc99a169882cbdd2e",
+    "url": "/eishow-alpha/static/js/main.7cf25012.chunk.js"
+  },
+  {
+    "revision": "cc29af1f86e2addf8beb",
+    "url": "/eishow-alpha/static/js/runtime-main.d9ddae80.js"
+  },
+  {
+    "revision": "eeccebcca5a4efbe1da731ab04fa02a9",
+    "url": "/eishow-alpha/static/media/back.eeccebcc.svg"
+  },
+  {
+    "revision": "45520ac91b09988df7feb8aa0c253660",
+    "url": "/eishow-alpha/static/media/eishow-lite.45520ac9.webm"
+  },
+  {
+    "revision": "8e3bf7af00cc2b50b17f8eb0776e05cd",
+    "url": "/eishow-alpha/static/media/logo.8e3bf7af.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/eishow-alpha/precache-manifest.7d4e0c80eee6057514f2e2db36d5fd0a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eishow-alpha/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
